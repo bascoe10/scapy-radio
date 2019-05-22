4a84cf9fcb6e9586e7c99b754e18ceaa03b718df
Sesay, Abass <abass_sesay@comcast.com>
2019-05-22 17:10:22 -0400

    add local blocks path

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ifconfig eth1 192.168.10.1
+sudo apt-get install mercurial
+hg clone https://bitbucket.org/cybertools/scapy-radio
+cd scapy-radio/
+sudo apt-get install gnuradio-dev libboost-dev liblog4cpp5-dev doxygen swig cmake pkg-config -y
+./install blocks
+./install.sh grc
+./install.sh scapy
+
+echo "[grc]\nlocal_blocks_path=/usr/local/share/gnuradio/grc/blocks" > ~/.gnuradio/config.conf
