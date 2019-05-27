4b5dff8550c45fcb1997a05588251ea8a3b452e5
Sesay, Abass <abass_sesay@comcast.com>
2019-05-26 21:24:18 -0400

    add grc for fm transmitter and receiver

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 ifconfig eth1 192.168.10.1
 sudo apt-get install gnuradio-dev libboost-dev liblog4cpp5-dev doxygen swig cmake pkg-config -y
-./install blocks
-./install.sh grc
-./install.sh scapy
+#./install blocks
+#./install.sh grc
+#./install.sh scapy
 
 echo "[grc]\nlocal_blocks_path=/usr/local/share/gnuradio/grc/blocks" > ~/.gnuradio/config.conf
