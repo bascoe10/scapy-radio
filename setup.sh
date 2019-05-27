96ca33720bf20de33283efbdd0772a385dfab4eb
Sesay, Abass <abass_sesay@comcast.com>
2019-05-26 21:31:01 -0400

    add gtk install

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 ifconfig eth1 192.168.10.1
-sudo apt-get install gnuradio-dev libboost-dev liblog4cpp5-dev doxygen swig cmake pkg-config -y
+sudo apt-get install gnuradio-dev libboost-dev liblog4cpp5-dev doxygen swig cmake pkg-config xterm libcanberra-gtk-module libcanberra-gtk3-module -y
 #./install blocks
 #./install.sh grc
 #./install.sh scapy
