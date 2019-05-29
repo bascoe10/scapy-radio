26c22c53e4e497bb96bdbdfd8efdb601421d6a51
Sesay, Abass <abass_sesay@comcast.com>
2019-05-29 17:36:22 -0400

    updated sensor

--- utils/Zwave/sensor.py
+++ utils/Zwave/sensor.py
@@ -1,19 +1,25 @@
 import random
-
-switch_radio_protocol("zwave")
-
-hid = 13268531
-
-gpkt = GnuradioPacket(proto=1)
-zpkt = ZWaveReq(homeid=hid, crc=None)
-znop_pkt = gpkt/zpkt/ZWaveNOP()
-
-zcmd_pkt = gpkt/zpkt/ZWaveSwitchBin()
-zcmd_pkt[ZWaveSwitchBin].val = "ON"
+from scapy.all import *
 
 
-while True:
-    if 5 == random.randint(1, 10):
-        srradio(zcmd_pkt)
-    else:
-        srradio(znop_pkt)
+def main():
+    switch_radio_protocol("zwave")
+
+    hid = 13268531
+
+    gpkt = GnuradioPacket(proto=1)
+    zpkt = ZWaveReq(homeid=hid, crc=None)
+    znop_pkt = gpkt/zpkt/ZWaveNOP()
+
+    zcmd_pkt = gpkt/zpkt/ZWaveSwitchBin()
+    zcmd_pkt[ZWaveSwitchBin].val = "ON"
+
+
+    while True:
+        if 5 == random.randint(1, 10):
+            srradio(zcmd_pkt)
+        else:
+            srradio(znop_pkt)
+
+if __name__ == '__main__':
+    main()
